d86885c3b1680308c762d068978ea91cb09c1ede
gerraywang <wang_yuquan@retail-ai.jp>
2024-01-20 13:46:33 +0900

    first commit

---
+++ src/main/kotlin/app/wyq/learning_kotlin/LearningKotlinApplication.kt
@@ -0,0 +1,11 @@
+package app.wyq.learning_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/app/wyq/learning_kotlin/LearningKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.wyq.learning_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
