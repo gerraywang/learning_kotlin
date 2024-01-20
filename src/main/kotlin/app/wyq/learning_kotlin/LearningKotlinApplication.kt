1ac04ed9988c69e4e897e4babc9220a18bf3d923
gerraywang <wang_yuquan@retail-ai.jp>
2024-01-20 15:53:02 +0900

    Create a controller

--- src/main/kotlin/app/wyq/learning_kotlin/LearningKotlinApplication.kt
+++ src/main/kotlin/app/wyq/learning_kotlin/LearningKotlinApplication.kt
@@ -2,10 +2,19 @@ package app.wyq.learning_kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class LearningKotlinApplication
 
 fun main(args: Array<String>) {
 	runApplication<LearningKotlinApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
