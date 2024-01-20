7678a4de48865fd3a79eb73f016b6e99a57f3daf
gerraywang <wang_yuquan@retail-ai.jp>
2024-01-20 16:14:00 +0900

    Add a data class to Spring Boot project

--- src/main/kotlin/app/wyq/learning_kotlin/LearningKotlinApplication.kt
+++ src/main/kotlin/app/wyq/learning_kotlin/LearningKotlinApplication.kt
@@ -17,4 +17,13 @@ fun main(args: Array<String>) {
 class MessageController {
     @GetMapping("/")
     fun index(@RequestParam("name") name: String) = "Hello, $name!"
+
+    @GetMapping("/id/")
+    fun index() = listOf(
+            Message("1", "Hello!"),
+            Message("2", "Bonjour!"),
+            Message("3", "Privet!"),
+    )
 }
+
+data class Message(val id: String?, val text: String)
